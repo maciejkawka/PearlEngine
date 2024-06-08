1b0ae7bf0eea5bbc88216bab6c2d758fd9148d82
Maciej Kawka <maciej.kawka3a@gmail.com>
2024-06-08 14:34:49 +0200

    Updated GLM dependency

--- Pearl/Engine/include/Engine/Core/Math/Math.h
+++ Pearl/Engine/include/Engine/Core/Math/Math.h
@@ -3,11 +3,12 @@
 #define GLM_FORCE_CXX17
 #define GLM_FORCE_INLINE
 #define GLM_FORCE_ALIGNED
+#define GLM_ENABLE_EXPERIMENTAL
+
 #include"glm/glm.hpp"
 #include"glm/gtc/quaternion.hpp"
 #include"glm/gtx/quaternion.hpp"
 #include "glm/gtc/type_ptr.hpp"
-#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/hash.hpp>
 
 namespace PrCore {
